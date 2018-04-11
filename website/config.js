7b21b2ba4c4f9232144beb5bdf29863081717192
Douglas Hipocreme <douglas@hipocreme.com.br>
2018-04-11 11:35:22 -0300

    gadcoin-pool commit

---
+++ website/config.js
@@ -0,0 +1,25 @@
+var api = "https://api.gadcoin.com.br";
+
+var api_blockexplorer = "https://apiblock.gadcoin.com.br";
+
+var poolHost = "pool.gadcoin.com.br";
+
+var irc = "";
+
+var email = "support@gadcoin.com.br";
+
+var cryptonatorWidget = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR"];
+
+var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
+
+var blockchainExplorer = "https://blockchain.gadcoin.com.br";
+
+var transactionExplorer = "https://blockchain.gadcoin.com.br";
+
+var themeCss = "themes/default-theme.css";
+
+var networkStat = {
+    "gad": [
+        ["pool.gadcoin.com.br", "pool.gadcoin.com.br:3333"]
+    ]
+};
